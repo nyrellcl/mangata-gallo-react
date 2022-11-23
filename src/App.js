17d8d90a8be5df0062bf377f1555e4df941c19b5
nyrellcl <nyrell.leonor2000@gmail.com>
2022-11-22 22:38:12 -0800

    hero section component commented out router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react'
 import Nav from './components/Nav';
-import { Route, Routes } from 'react-router-dom'
+//import { Route, Routes } from 'react-router-dom'
 import HeroSection from './components/Hero-Section/HeroSection';
 import Footer from "./components/Footer"
 import ProductPage from './components/Product-Page/ProductPage';
@@ -13,9 +13,10 @@ function App() {
       <Nav first="Home"
         second="About"
         third="Products" />
-      <Routes>
-        <Route path="/" component={<HeroSection />} />
-      </Routes>
+        <HeroSection />
+      {/*<Routes>
+        <Route path="/" component={} />
+  </Routes>*/}
       <ProductPage />
       <Form />
       <Footer />
