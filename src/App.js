1424372bae901ea6a1051d1a87bd834e71ba5540
nyrellcl <nyrell.leonor2000@gmail.com>
2022-12-01 16:45:27 -0800

    router edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HeroSection from './components/pages/home/Hero-Section/HeroSection';
 import Footer from "./components/Footer"
 import About from './components/pages/about/About'
@@ -9,15 +9,16 @@ import ProductMain from './components/pages/products/ProductMain';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<HeroSection />} />
+        <Route path="/" exact element={<HeroSection />} />
         <Route path='/about' element={<About />} />
         <Route path="/products" element={<ProductMain />} />
       </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
+
 
 
 
