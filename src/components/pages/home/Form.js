05fe448d6d91d82ce816b6ec1964f4f8b6e1a266
nyrellcl <nyrell.leonor2000@gmail.com>
2022-12-07 09:31:35 -0800

    added controlled form

--- src/components/pages/home/Form.js
+++ src/components/pages/home/Form.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import "./Form-Sass/Form.css"
 import {Button} from "../../Button"
+import {useState} from 'react'
 
 function Form() {
+    const [email, setEmail] = useState("");
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setEmail("")
+        console.log("Submitted")
+    }
     return (
         <section className='form-section'>
             <div className='subscribe-heading'>
                 <h4>Subscribe to our newsletter</h4>
             </div>
-            <form action='GET' className='form-container'>
+            <form action='GET' className='form-container' onSubmit={handleSubmit}>
                 <label htmlFor="subscribe">E-mail</label>
-                <input type="email" id='subscribe' name='subscribe' placeholder='Enter your email...'></input>
-                <Button buttonStyle='btn-tertiary' buttonSize='btn-submit' type='submit'>Submit</Button>
+                <input value={email} type="email" id='subscribe' name='subscribe' placeholder='Enter your email...' onChange={(e) => setEmail(e.target.value)}></input>
+                <Button disabled={!email} buttonStyle='btn-tertiary' buttonSize='btn-submit' type='submit'>Submit</Button>
             </form>
         </section>
     )
